6cb46c7fbdc14b7be59e090423a76d99dc0efea0
AravindaVijay <143136183+AravindaVijay@users.noreply.github.com>
2024-12-06 19:04:57 -0500

    update from main

--- Code/models/resume_train_preprocess_test.py
+++ Code/models/resume_train_preprocess_test.py
@@ -44,10 +44,10 @@ def preprocess_random_samples(dataset, headers, num_samples=100):
 # got all these possible headers from the below code, can add if any missed
 section_headers = [
     "Professional Summary", "Summary","SKILL SET", "Objective", "Experience", "Work History",
-    "Education", "Hobbies","SOFTWARE SKILLS","Education Details","Training attended","TECHNICAL EXPERTISE","Technical Expertise",  "SKILLS","CORE COMPETENCIES", "Skills", "Certifications", "Projects", "Accomplishments",
-    "Affiliations","TECHNICALSKILLS","Technical Summary","Computer skills","Key Skills","TECHNICAL STRENGTHS","Technical Skill Set",  "KEY COMPETENCIES","PERSONAL SKILLS","IT SKILLS","Skill Set","Areas of expertise","AREA OF EXPERTISE", "Interests", "Languages", "References", "Technical Skills"
+    "Education", "Hobbies","SOFTWARE SKILLS","Technical Skill Details","Education Details","Training attended","TECHNICAL EXPERTISE","Technical Expertise",  "SKILLS","CORE COMPETENCIES", "Skills", "Certifications", "Projects", "Accomplishments",
+    "Affiliations","Company Details","TECHNICALSKILLS","Technical Summary","Computer skills","Key Skills","TECHNICAL STRENGTHS","Technical Skill Set",  "KEY COMPETENCIES","PERSONAL SKILLS","IT SKILLS","Skill Set","Areas of expertise","AREA OF EXPERTISE", "Interests", "Languages", "References", "Technical Skills"
 ]
-
+#%%
 dataset = load_dataset("InferencePrince555/Resume-Dataset")
 
 preprocessed_lines = preprocess_random_samples(dataset, section_headers, num_samples=100)
@@ -72,10 +72,10 @@ for idx, preprocessed in enumerate(preprocessed_first_100, start=1):
 #     print(f"Preprocessed Resume {idx}:\n")
 #     print(preprocessed)
 #     print("\n" + "="*80 + "\n")
-
-
-
-
+#%%
+text="ACCOUNTANT Summary Seasoned professional accounting professional skilled at managing multiple accounts across diverse financial and HR systems An analytical leader with outstanding leadership qualities excellent communication skills and an attention to details Experience Accountant 04 2015 to Current Company Name City State Responsible for the management of multi departmental account reconciliations including cash receipts general Ledger reports as well as reconciling vendor invoices Generated weekly monthly semi annual budget based on company s sales projections Analyzed actuals against revenue generated from various departments such as payroll tax payables medical insurance etc Ensuring compliance with internal Sarbanes Oxley SOX 404 procedures Performed audit activities involving auditing audits bank reconciliation income statements and treasury functions in accordance with generally accepted accounting practices Reviewed Bank Reconciliation Statement EAR P L Accounts Payable Reports Monitored General Ledger Fixed Asset Accounting which includes entering data into a system Assisted Controller accountant by preparing work schedules ensuring proper documentation is being maintained Served as primary contact for client regarding issues related to payment status Prepared expense forecasts that include variance variances adjusting gross margin while maintaining accuracy Key Accomplishments Manage all year end operating expenses within established guidelines Schedule month end closing reviews Maintained over 150 000 project payments annually Collaborated extensively with Finance Department team members during special projects Successfully managed 6 separate acquisitions simultaneously through successful divestiture Oversaw new employee orientation training program consisting of 25 employees Education 2013 BACHELORS OF BUSINESS OPERATION APPLIED SCIENCE Business Administration University of Puerto Rico City State Certifications FINANCIAL AND PERSONAL ACCOUNTANT August 2011 to May 2014 Company Name City State Gathered analyzed and resolved key operational issues Developed detailed business processes Evaluated cost reduction opportunities Worked effectively with upper management to define requirements Sustained positive relations with clients vendors and external partners Established clear expectations about operations and future goals Kept track of tasks completed throughout entire tenure Handled escalated situations when necessary Provided exceptional customer service Resolved complex technical issues accurately and expediently Actively pursued solutions provided exemplary customer service Negotiated pricing agreements contracts fees and other third party products Processed Purchase Orders via QuickBooks Salesforce Confidentiality Verification Report AFRA Bookkeeper November 2010 to February 2011 Company Name City State Operated electronic filing system Took charge of day to day administrative duties Conducted quality assurance tests Implemented corrective care methods Answered phone calls wrote correspondence Completed daily inventory counts Wrote automated test cases Executed adhoc reporting Used statistical analysis to develop marketing strategies Communicated product changes information risks and trends to appropriate stakeholders Tr"
+cleaned=preprocess_resume(text,section_headers)
+print(cleaned)
 # %%
 # getting all possible headers with regex
 dataset = load_dataset("InferencePrince555/Resume-Dataset")
